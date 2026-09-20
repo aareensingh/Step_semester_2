ef0d14c313e0826b1ec0647ede2f8fdb833f5b61
aareensingh <aareensingh@gmail.com>
2026-09-20 18:11:10 +0530

    Create TwoDToOneD.java

---
+++ Weekly_Assignments/TwoDToOneD.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+class TwoDToOneD {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int rows = input.nextInt();
+        int columns = input.nextInt();
+
+        int[][] matrix = new int[rows][columns];
+        int[] array = new int[rows * columns];
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                matrix[i][j] = input.nextInt();
+            }
+        }
+
+        int index = 0;
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                array[index] = matrix[i][j];
+                index++;
+            }
+        }
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.println(array[i]);
+        }
+    }
+}
