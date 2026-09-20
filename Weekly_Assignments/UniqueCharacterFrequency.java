3f214c3f25cb409f663b4f515bcb24068a6b3de1
aareensingh <aareensingh@gmail.com>
2026-09-20 20:45:59 +0530

    Create UniqueCharacterFrequency.java

---
+++ Weekly_Assignments/UniqueCharacterFrequency.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+class UniqueCharacterFrequency {
+    public static char[] uniqueCharacters(String text) {
+        char[] temp = new char[text.length()];
+        int count = 0;
+
+        for (int i = 0; i < text.length(); i++) {
+            boolean found = false;
+
+            for (int j = 0; j < i; j++) {
+                if (text.charAt(i) == text.charAt(j)) {
+                    found = true;
+                    break;
+                }
+            }
+
+            if (!found)
+                temp[count++] = text.charAt(i);
+        }
+
+        char[] result = new char[count];
+
+        for (int i = 0; i < count; i++)
+            result[i] = temp[i];
+
+        return result;
+    }
+
+    public static String[][] findFrequency(String text) {
+        char[] unique = uniqueCharacters(text);
+        String[][] result = new String[unique.length][2];
+
+        for (int i = 0; i < unique.length; i++) {
+            int count = 0;
+
+            for (int j = 0; j < text.length(); j++) {
+                if (unique[i] == text.charAt(j))
+                    count++;
+            }
+
+            result[i][0] = String.valueOf(unique[i]);
+            result[i][1] = String.valueOf(count);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String text = sc.nextLine();
+
+        String[][] result = findFrequency(text);
+
+        System.out.println("Character\tFrequency");
+
+        for (int i = 0; i < result.length; i++)
+            System.out.println(result[i][0] + "\t\t" + result[i][1]);
+    }
+}
