a0e26456b0b2f9a67ee33ed7f05b2ac276826c52
aareensingh <aareensingh@gmail.com>
2026-09-20 20:41:40 +0530

    Create VotingEligibility1.java

---
+++ Weekly_Assignments/VotingEligibility1.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+class VotingEligibility {
+    public static int[] getAges(int n) {
+        Scanner sc = new Scanner(System.in);
+        int[] ages = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            System.out.print("Enter age of student " + (i + 1) + ": ");
+            ages[i] = sc.nextInt();
+        }
+
+        return ages;
+    }
+
+    public static String[][] checkVoting(int[] ages) {
+        String[][] result = new String[ages.length][2];
+
+        for (int i = 0; i < ages.length; i++) {
+            result[i][0] = String.valueOf(ages[i]);
+
+            if (ages[i] >= 18)
+                result[i][1] = "true";
+            else
+                result[i][1] = "false";
+        }
+
+        return result;
+    }
+
+    public static void display(String[][] result) {
+        System.out.println("Age\tCan Vote");
+
+        for (int i = 0; i < result.length; i++)
+            System.out.println(result[i][0] + "\t" + result[i][1]);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = 10;
+        int[] ages = getAges(n);
+        String[][] result = checkVoting(ages);
+
+        display(result);
+    }
+}
