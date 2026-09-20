0b1397043dcd9bc1ebf4919ddcefc0718f3028d8
aareensingh <aareensingh@gmail.com>
2026-09-20 17:40:46 +0530

    Create DivisibleBy5.java

---
+++ Weekly_Assignments/DivisibleBy5.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class DivisibleBy5 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        int number = input.nextInt();
+
+        System.out.println("Is the number " + number + " divisible by 5? " + (number % 5 == 0));
+    }
+}
