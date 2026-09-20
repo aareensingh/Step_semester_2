b4e9c6b12176f73a7bbc30ea16a57104fcece260
aareensingh <aareensingh@gmail.com>
2026-09-20 20:32:43 +0530

    Create NumberFormatExceptionDemo.java

---
+++ Weekly_Assignments/NumberFormatExceptionDemo.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+class NumberFormatExceptionDemo {
+    public static void generateException(String text) {
+        int number = Integer.parseInt(text);
+        System.out.println(number);
+    }
+
+    public static void handleException(String text) {
+        try {
+            int number = Integer.parseInt(text);
+            System.out.println(number);
+        } catch (NumberFormatException e) {
+            System.out.println("NumberFormatException handled");
+        } catch (RuntimeException e) {
+            System.out.println("RuntimeException handled");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String text = input.next();
+
+        handleException(text);
+    }
+}
