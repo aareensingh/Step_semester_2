e1232ca52ff9b0bb348411176f1f1ab3ece50b10
aareensingh <aareensingh@gmail.com>
2026-09-20 20:31:39 +0530

    Create StringIndexOutOfBoundsDemo.java

---
+++ Weekly_Assignments/StringIndexOutOfBoundsDemo.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+class StringIndexOutOfBoundsDemo {
+    public static void generateException(String text) {
+        System.out.println(text.charAt(text.length()));
+    }
+
+    public static void handleException(String text) {
+        try {
+            System.out.println(text.charAt(text.length()));
+        } catch (StringIndexOutOfBoundsException e) {
+            System.out.println("StringIndexOutOfBoundsException handled");
+        } catch (RuntimeException e) {
+            System.out.println("RuntimeException handled");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String text = input.next();
+
+        handleException(text);
+    }
+}
