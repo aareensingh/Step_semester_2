f29c6e7221b612ab0b16c2ba59a3773798570901
aareensingh <aareensingh@gmail.com>
2026-09-20 21:11:34 +0530

    Create UC4 HelloApp.java

---
+++ HELLO_APP/UC4 HelloApp.java
@@ -0,0 +1,18 @@
+public class HelloApp {
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            StringBuilder names = new StringBuilder();
+
+            for (int i = 0; i < args.length; i++) {
+                if (i > 0) {
+                    names.append(", ");
+                }
+                names.append(args[i]);
+            }
+
+            System.out.println("Hello, " + names + "!");
+        } else {
+            System.out.println("Hello, World!");
+        }
+    }
+}
